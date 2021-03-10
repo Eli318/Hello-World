691cd4a64d7ec6520d72696cba134e5fc55050c2
Eli318 <eliezerroseman@gmail.com>
2021-03-11 00:04:27 +0200

    second commit

---
+++ add.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Mar 10 21:05:11 2021
+
+@author: Eliezer
+"""
+
+a = int(input("Please enter integer: "))
+b = int(input("Please enter another integer: "))
+print(a+b)
